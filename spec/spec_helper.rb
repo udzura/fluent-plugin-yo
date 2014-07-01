824c71936d73dddba27b45538077f7e51fee16a2
Uchio KONDO <udzura@pepabo.com>
2014-07-01 09:58:05 +0900

    Tidy up spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.order = :random
+
+  config.expect_with :rspec do |expectations|
+    # http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    # http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+    mocks.syntax = :expect
+
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended.
+    mocks.verify_partial_doubles = true
+  end
+end
