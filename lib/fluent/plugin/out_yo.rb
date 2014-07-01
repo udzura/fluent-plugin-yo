a0ffcb811ae92badc4dbefcd98bcca0367db0d14
Uchio KONDO <udzura@pepabo.com>
2014-07-01 10:01:40 +0900

    Wrong format plugin

--- lib/fluent/plugin/out_yo.rb
+++ lib/fluent/plugin/out_yo.rb
@@ -2,29 +2,31 @@ require "fluent/plugin/yo/version"
 
 module Fluent
   class YoOutput < Fluent::Output
+    Fluent::Plugin.register_output('ikachan', self)
+
     config_param :api_key, :string, :default => nil
-  end
 
-  def initialize
-    super
-    require 'yo'
-  end
-
-  def configure(conf)
-    super
-    raise Fluent::ConfigError, 'Visit http://yoapi.justyo.co/ and get an api key' if !@api_key or @api_key.empty?
-    @yo = Yo.new(@api_key)
-  end
-
-  def start; end
-  def shutdown; end
-
-  def emit(tag, es, chain)
-    begin
-      @yo.yo
-    rescue => e
-      raise Fluent::ConfigError, "Maybe Yo config is mistaken: #{e.class}, #{e.message}"
+    def initialize
+      super
+      require 'yo'
     end
-    chain.next
+
+    def configure(conf)
+      super
+      raise Fluent::ConfigError, 'Visit http://yoapi.justyo.co/ and get an api key' if !@api_key or @api_key.empty?
+      @yo = Yo.new(@api_key)
+    end
+
+    def start; end
+    def shutdown; end
+
+    def emit(tag, es, chain)
+      begin
+        @yo.yo
+      rescue => e
+        raise Fluent::ConfigError, "Maybe Yo config is mistaken: #{e.class}, #{e.message}"
+      end
+      chain.next
+    end
   end
 end
